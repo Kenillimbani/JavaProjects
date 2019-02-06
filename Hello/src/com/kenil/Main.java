9b6977d2c6ca84de8e7f15b16918580ac2f095a5
kenil <kenil@DESKTOP-SU4KQBG>
2019-02-06 21:39:06 +0530

    Updating main file

--- Hello/src/com/kenil/Main.java
+++ Hello/src/com/kenil/Main.java
@@ -3,9 +3,8 @@ package com.kenil;
 public class Main {
 
 	public static void main(String[] args) {
-		//reading values from text file
-		
-		
+		//reading values from text file		
+		System.out.println("Hello Kenil !!");
 	}
 
 }
